13a65bbe77bb72d8b48d264bdb1aa621dede0873
Sebastian Spratt <seb.spratt@gmail.com>
2021-06-29 17:35:08 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AppRoutingModule } from './app-routing.module';
-import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 import { AppComponent } from './core/app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +18,6 @@ import { environment } from 'src/environments/environment';
     BrowserAnimationsModule,
     environment.production ? [] : AkitaNgDevtools.forRoot({ logTrace: false, maxAge: 20})
   ],
-  providers: [{ provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/services/search.service.spec.ts
+++ src/app/shared/services/search.service.spec.ts
@@ -13,4 +13,16 @@ describe('SearchService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should not accept filter strings that are not substrings of the data', () => {
+    expect(service.filter('test', 'be')).toBeFalse();
+  });
+
+  it('should accept filter strings that are substrings of the data', () => {
+    expect(service.filter('abc', 'ab')).toBeTrue();
+  });
+
+  it('should accept the empty string', () => {
+    expect(service.filter('xyz', '')).toBeTrue();
+  });
 });
